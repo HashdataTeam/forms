550b2ff9041f7d91ebbd02e5a3356a876013939b
Modestino <modestino@gmail.com>
2024-09-17 16:49:25 -0300

    gh-pages

---
+++ build.sh
@@ -0,0 +1,9 @@
+rm -rf _site
+bundle exec jekyll build
+rm -rf /tmp/_site
+cp -a _site /tmp
+git checkout gh-pages
+cp -a /tmp/_site/* .
+git add .
+git commit -m "Update site"
+git push origin gh-pages
